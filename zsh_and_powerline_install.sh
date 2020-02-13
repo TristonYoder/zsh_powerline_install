b0a490b61fa2c7433a499866316ef6eba290bd2c
Triston Yoder <dev@triston.me>
2020-02-13 11:19:07 -0500

    First version of the script.
    Quick & Dirty Script that:
    installs zsh, powerline fonts, and powerline9k
    Sets the zsh config to use powerline9k
    sets zsh as the default shell
    starts zsh

---
+++ zsh_and_powerline_install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# Let's get started
+sudo apt-get update
+sudo apt upgrade
+
+#install everything
+sudo apt install zsh
+sudo apt-get install powerline fonts-powerline
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+#create config
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+#Change theme to powerlevel9k
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel9k\/powerlevel9k"/g' .zshrc
+
+#Override 256 Color Error
+sed -i '1i#Ignore 256 Color Error\nexport TERM="xterm-256color"\n' .zshrc
+
+#change shell
+chsh -s /bin/zsh
+
+#update
+cd .oh-my-zsh
+upgrade_oh_my_zsh
+
+#add syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
+
+#Cleanup & Start
+zsh
+cd ~
+clear
