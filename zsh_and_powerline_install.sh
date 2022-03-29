52d801d2be81b19ad8db6796a9bf9a54b48fe2b1
Triston Yoder <dev@triston.me>
2022-03-29 16:30:24 -0400

    Update zsh_and_powerline_install.sh
    use prebuilt config instead of mucking about with the config in the script

--- zsh_and_powerline_install.sh
+++ zsh_and_powerline_install.sh
@@ -9,27 +9,20 @@ sudo apt-get install powerline fonts-powerline
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-#create config
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+#add syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
 
-#Change theme to powerlevel9k
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel9k\/powerlevel9k"/g' .zshrc
-
-#Override 256 Color Error & Set Locale
-sed -i '1i#Ignore 256 Color Error\nexport TERM="xterm-256color"\n\n#Set Locale\nexport LC_ALL=en_US.UTF-8\nexport LANG=en_US.UTF-8\n' .zshrc
+#get config
+sudo wget https://raw.githubusercontent.com/TristonYoder/zsh_powerline_install/master/.zshrc -P ~/
 
 #change shell
 chsh -s /bin/zsh
 
-#update
+#Start and Update
+zsh
 cd .oh-my-zsh
 upgrade_oh_my_zsh
 
-#add syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
-echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
-
-#Cleanup & Start
-zsh
+#Cleanup
 cd ~
 clear
